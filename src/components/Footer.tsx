2730d32d6831002cf280cd202f62839ac2cd5976
Vanshika Prakash <vanshikaprakash05@gmail.com>
2025-07-14 10:59:00 +0000

    the back to top button is hiding it

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="py-6 md:px-8 md:py-0 border-t">
+    <footer className="py-6 md:px-8 md:py-0 border-t pb-24 md:pb-6">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <nav className="flex gap-4 sm:gap-6 text-sm text-muted-foreground">
             <Link href="/about" className="transition-colors hover:text-foreground">About</Link>
